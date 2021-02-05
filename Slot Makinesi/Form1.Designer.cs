0da4971ddbc1ef09c0f6041afe5a10d9438669c5
mervecbn <merve4417@gmail.com>
2021-02-05 14:30:28 +0300

    Proje

---
+++ Slot Makinesi/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Slot_Makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.jetonat = new System.Windows.Forms.Button();
+            this.başla = new System.Windows.Forms.Button();
+            this.durdur = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(62, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(158, 145);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(276, 57);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(158, 145);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(510, 57);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(158, 145);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // jetonat
+            // 
+            this.jetonat.BackColor = System.Drawing.Color.Purple;
+            this.jetonat.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.jetonat.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.jetonat.Location = new System.Drawing.Point(62, 260);
+            this.jetonat.Name = "jetonat";
+            this.jetonat.Size = new System.Drawing.Size(122, 34);
+            this.jetonat.TabIndex = 3;
+            this.jetonat.Text = "Jeton At";
+            this.jetonat.UseVisualStyleBackColor = false;
+            this.jetonat.Click += new System.EventHandler(this.jetonat_Click);
+            // 
+            // başla
+            // 
+            this.başla.BackColor = System.Drawing.Color.Purple;
+            this.başla.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.başla.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.başla.Location = new System.Drawing.Point(62, 324);
+            this.başla.Name = "başla";
+            this.başla.Size = new System.Drawing.Size(122, 34);
+            this.başla.TabIndex = 4;
+            this.başla.Text = "Başla";
+            this.başla.UseVisualStyleBackColor = false;
+            this.başla.Click += new System.EventHandler(this.başla_Click);
+            // 
+            // durdur
+            // 
+            this.durdur.BackColor = System.Drawing.Color.Purple;
+            this.durdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.durdur.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.durdur.Location = new System.Drawing.Point(62, 393);
+            this.durdur.Name = "durdur";
+            this.durdur.Size = new System.Drawing.Size(122, 34);
+            this.durdur.TabIndex = 5;
+            this.durdur.Text = "Durdur";
+            this.durdur.UseVisualStyleBackColor = false;
+            this.durdur.Click += new System.EventHandler(this.durdur_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(465, 324);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Puan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.durdur);
+            this.Controls.Add(this.başla);
+            this.Controls.Add(this.jetonat);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button jetonat;
+        private System.Windows.Forms.Button başla;
+        private System.Windows.Forms.Button durdur;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Slot Makinesi/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slot_Makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (puan==1000)
+            {
+                Close();
+            }
+        }
+
+       string[] resimler = Directory.GetFiles(@"C:\Users\ASUS\Desktop\img", "*.png");
+        
+       
+        int jetonadet;
+        int puan = 0;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            
+            pictureBox1.ImageLocation= resimler[random.Next(0, resimler.Length)];
+            pictureBox2.ImageLocation = resimler[random.Next(0, resimler.Length)];
+            pictureBox3.ImageLocation = resimler[random.Next(0, resimler.Length)];
+           
+
+
+        }
+
+        private void başla_Click(object sender, EventArgs e)
+        {
+            if (jetonadet > 0)
+            {
+                timer1.Start();
+                durdur.Enabled = true;
+
+            }
+            else
+            {
+                MessageBox.Show("Jeton Atmalısınız!");
+            }
+        }
+
+        private void durdur_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Stop();
+            if (pictureBox1 == pictureBox2 && pictureBox2 ==pictureBox3 )
+            {
+                puan += 100;
+                label1.Text = puan.ToString();
+            }
+            else if (pictureBox1 == pictureBox2 || pictureBox2 == pictureBox3 || pictureBox2 == pictureBox3)
+            {
+                puan += 50;
+                label1.Text = puan.ToString();
+
+            }
+        }
+
+        private void jetonat_Click(object sender, EventArgs e)
+        {
+            jetonadet += 1;
+        }
+    }
+}
